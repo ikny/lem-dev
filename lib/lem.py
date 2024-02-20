502dd75b6d706b32b348b983672b10920de7d654
ikny <vojtech.mikula@email.cz>
2024-02-20 14:56:37 +0100

    Move the post_production method to stream manager.

--- lib/lem.py
+++ lib/lem.py
@@ -1,5 +1,5 @@
 # types
-from typing import Any
+from typing import Any, Optional
 import numpy.typing as npt
 # libs
 import sounddevice as sd
@@ -77,57 +77,11 @@ class Lem():
             bool: Passes the value returned by the post_production method.
         """
         recorded_track = self._stream_manager.stop_recording()
-        return self.post_production(recorded_track=recorded_track)
-
-    def post_production(self, recorded_track: RecordedTrack) -> bool:
-        """Cut/fill newly recorded track to whole bpm and add it to tracks.
-
-        Args:
-            recorded_track (npt.NDArray[DTYPE]): The audio data to be modified.
-
-        Returns:
-            bool: True if the track was long at least one beat, thus it was actually added into tracks. 
-            False if the rounding resulted in an empty track.
-        """
-        # TODO: solve this typing mess
-        first: int = recorded_track.first_frame_time  # type: ignore
-        start: int = recorded_track.start_rec_time  # type: ignore
-        stop: int = recorded_track.stop_rec_time  # type: ignore
-        data = recorded_track.data
-        length = len(data)
-
-        half_beat = int(self._len_beat/2)
-
-        print(f"beat, halfbeat: {self._len_beat, half_beat}")
-        print(f"first: {first}")
-        print(f"start: {start}")
-        print(f"stop: {stop}")
-        print(f"length: {length}")
-
-        # TODO: the data were not long in beats (lb: 48109, ld: 48282)
-        if start - first < half_beat:
-            start = 0
-        else:
-            start = self._len_beat
-            first += self._len_beat
-        print(f"rounded start: {start}")
-
-        if (stop - first) % self._len_beat < half_beat:
-            stop = length - self._len_beat
-        else:
-            stop = length
-        print(f"rounded stop: {stop}")
-
-        data = data[start:stop]
-        print(f"rounded data len: {len(data)}")
-
-        if len(data):
-            print(f"first again: {first}")
-            self._tracks.append(PlayingTrack(
-                data=data, playing_from_frame=first))
-            self._update_tracks()
-            return True
-        return False
+        if not recorded_track:
+            return False
+        self._tracks.append(recorded_track)
+        self._update_tracks()
+        return True
 
     def delete_track(self, idx: int) -> None:
         """Removes the track on index idx+1, because the first track is the metronome sample.
@@ -194,29 +148,6 @@ class LoopStreamManager():
         except AttributeError:
             return
 
-    def start_recording(self) -> None:
-        """Set the _recording flag to True. This works because 
-        the callback method checks the flag when deciding whether to store indata.
-        """
-        self._event_queue.push(UserRecordingEvents.START)
-
-    def stop_recording(self) -> RecordedTrack:
-        # TODO: check all the documentation for correct types
-        """Sets the _recording flag to False. 
-        Waits until the recording stops and afterwards prepares _recording_track for new recording.
-
-        Returns:
-            npt.NDArray[DTYPE]: The recorded track.
-        """
-        self._event_queue.push(UserRecordingEvents.STOP)
-        while True:
-            try:
-                recorded_track: RecordedTrack = self._recorded_tracks_queue.pop()
-                break
-            except IndexError:
-                sleep(0.001)
-        return recorded_track
-
     def update_tracks(self, tracks: list[PlayingTrack]) -> None:
         """Update its data in a thread safe manner using lock. First update the backup, 
         from which will the callback read while _tracks are being updated.
@@ -227,6 +158,78 @@ class LoopStreamManager():
         self._tracks_copy = self._tracks
         with self._tracks_lock:
             self._tracks = tracks
+
+    def start_recording(self) -> None:
+        """Set the _recording flag to True. This works because 
+        the callback method checks the flag when deciding whether to store indata.
+        """
+        self._event_queue.push(UserRecordingEvents.START)
+
+    def stop_recording(self) -> Optional[PlayingTrack]:
+        # TODO: check all the documentation for correct types
+        """Sets the _recording flag to False. 
+        Waits until the recording stops and afterwards prepares _recording_track for new recording.
+
+        Returns:
+            npt.NDArray[DTYPE]: The recorded track.
+        """
+        self._event_queue.push(UserRecordingEvents.STOP)
+        # TODO: async wait is probably supposed to be used here
+        while True:
+            if not self._recorded_tracks_queue.empty():
+                break
+            sleep(0.001)
+        raw_track: RecordedTrack = self._recorded_tracks_queue.pop()
+        recorded_track = self.post_production(raw_track)
+        return recorded_track
+
+    def post_production(self, recorded_track: RecordedTrack) -> Optional[PlayingTrack]:
+        """Cut/fill newly recorded track to whole bpm and add it to tracks.
+
+        Args:
+            recorded_track (npt.NDArray[DTYPE]): The audio data to be modified.
+
+        Returns:
+            bool: True if the track was long at least one beat, thus it was actually added into tracks. 
+            False if the rounding resulted in an empty track.
+        """
+        # TODO: solve this typing mess
+        first: int = recorded_track.first_frame_time  # type: ignore
+        start: int = recorded_track.start_rec_time  # type: ignore
+        stop: int = recorded_track.stop_rec_time  # type: ignore
+        data = recorded_track.data
+        length = len(data)
+
+        half_beat = int(self._len_beat/2)
+
+        print(f"beat, halfbeat: {self._len_beat, half_beat}")
+        print(f"first: {first}")
+        print(f"start: {start}")
+        print(f"stop: {stop}")
+        print(f"length: {length}")
+
+        # TODO: the data were not long in beats (lb: 48109, ld: 48282)
+        if start - first < half_beat:
+            start = 0
+        else:
+            start = self._len_beat
+            first += self._len_beat
+        print(f"rounded start: {start}")
+
+        if (stop - first) % self._len_beat < half_beat:
+            stop = length - self._len_beat
+        else:
+            stop = length
+        print(f"rounded stop: {stop}")
+
+        data = data[start:stop]
+        print(f"rounded data len: {len(data)}")
+
+        if len(data):
+            print(f"first again: {first}")
+            return PlayingTrack(
+                data=data, playing_from_frame=first)
+        return None
 
     def main(self) -> None:
         """Open a sounddevice stream and keep it active while flag _stream_active is true.
